be2e08512d2b92c7c3edefe83cac828bf06ac168
Thomas Schwark <schwarkthomas@outlook.de>
2021-12-11 19:12:57 +0100

    Added comments to the PinDefinitions.

--- src/PinDefinitions.h
+++ src/PinDefinitions.h
@@ -5,11 +5,11 @@
 #ifndef PIN_DEFINITIONS
 #define PIN_DEFINITIONS
 
-#define DATA_PIN D1
-#define CLOCK_PIN D2
+#define DATA_PIN D1         // GPIO5
+#define CLOCK_PIN D2        // GPIO4
 
-#define CPU_CLOCK_PIN D5
-#define IN_LATCH_PIN D6
-#define OUT_LATCH_PIN D7
+#define CPU_CLOCK_PIN D5    // GPIO14
+#define IN_LATCH_PIN D6     // GPIO12
+#define OUT_LATCH_PIN D7    // GPIO13
 
 #endif
